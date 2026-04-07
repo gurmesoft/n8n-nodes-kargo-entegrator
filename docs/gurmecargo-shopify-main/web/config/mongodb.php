559495e346236542c937e3f51d1e82dd7a43e40b
Fuat POYRAZ <fuat@gurmesoft.com>
2026-04-07 10:17:46 +0300

    docs

--- docs/gurmecargo-shopify-main/web/config/mongodb.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'mongodb_host' => env('MONGODB_HOST', ''),
-    'mongodb_username' => env('MONGODB_USERNAME', ''),
-    'mongodb_password' => env('MONGODB_PASSWORD', ''),
-];
--- docs/gurmecargo-shopify-main/web/resources/js/components/providers/index.js
+++
@@ -1,1 +0,0 @@
-export { PolarisProvider } from "./PolarisProvider";
